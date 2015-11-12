48b7852bb268c350338b5beaedd3b03884abc5ba
Alexander Hudkoff <alexus.web@gmail.com>
2015-11-12 06:23:08 +0300

    fixes urls and return false

--- www/_js/js.js
+++ www/_js/js.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
 		$(this).toggleClass("state_open");
 		$(".menu").toggleClass("state_open");
 		$(".subMenu, .menu__link.view_menuIcon").removeClass("state_open");
+		return false;
 	});
 
 	$(".menu__link.view_menuIcon").on('click', function(){
 		$(this).toggleClass("state_open");
 		$(".subMenu").toggleClass("state_open");
+		return false;
 	});
 
 

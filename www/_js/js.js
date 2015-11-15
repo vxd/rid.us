7df8019df9674a7059ebf44891754afd072b32f1
Alexander Hudkoff <alexus.web@gmail.com>
2015-11-15 22:08:13 +0300

    search

--- www/_js/js.js
+++ www/_js/js.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	// меню 
 	// ======================================
 
-
+	/* мобильное МЕНЮ */
 	$(".mobileMenuButton").on('click', function(){
 		$(this).toggleClass("state_open");
 		$(".menu").toggleClass("state_open");
@@ -13,14 +13,32 @@ $(document).ready(function() {
 		return false;
 	});
 
+	/* меню ЕЩЕ */
 	$(".menu__link.view_menuIcon").on('click', function(){
 		$(this).toggleClass("state_open");
 		$(".subMenu").toggleClass("state_open");
 		return false;
 	});
 
-
-
+	/* поле поиска */
+	$(".header__searchButton, .headerSearch__input").on('click', function(){
+		$(".headerSearch__form").fadeIn(220);
+		$(".headerSearch__input").focus();
+		return false;
+	});
+	$("body, button:not(.headerSearch__button)").on('click', function(){
+		$(".headerSearch__form").fadeOut(150);
+		$(".headerSearch__input").blur();
+		return false;
+	});
+	$(".headerSearch__button").on('click', function(){
+		console.log( "search:" + $(".headerSearch__input").val() )
+		return true;
+	});
+	$(window).resize(function(){
+		$(".headerSearch__form").fadeOut(50);
+		$(".headerSearch__input").blur();
+	});
 
 });
 

5a7c838b6850c9b703cf9f57500c57ec597175f2
Alexander Hudkoff <alexus.web@gmail.com>
2015-11-25 01:32:23 +0300

    slider on block page

--- www/_js/js.js
+++ www/_js/js.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
-
+    var swiper = new Swiper('.slider', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        // spaceBetween: 30,
+        centeredSlides: true,
+        // autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
 
 });
-
-
-
-
 
 
 
